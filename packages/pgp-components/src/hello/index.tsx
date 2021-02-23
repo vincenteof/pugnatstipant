81cf0f0aa8c36db86b15305f1a46ef476ebab4d2
vincenteof <vincenteof@qq.com>
2021-02-23 10:07:51 +0800

    feat: css test

--- packages/pgp-components/src/hello/index.tsx
+++ packages/pgp-components/src/hello/index.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import World from '../world'
 
 type HelloProps = {}
 
 const Hello: React.FC<HelloProps> = (props) => {
-  return <div className="hello">Hello, World</div>
+  return (
+    <div className="hello">
+      Hello, <World />
+    </div>
+  )
 }
 
 export default Hello
